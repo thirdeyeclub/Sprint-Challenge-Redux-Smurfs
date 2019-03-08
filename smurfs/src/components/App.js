a0364b117a599f868992785093b9c939bdf904e2
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-03-08 12:55:06 -0500

    wrote reducer functions

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import { bindActionCreators } from 'redux';
+import { connect } from "react-redux";
+import { fetchSmurfs, addSmurf} from './../actions/index';
+
+
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,12 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {FETCHING_SMURFS
+      , FETCHING_SMURFS_SUCCESS
+      , FETCHING_SMURFS_FAILURE
+      , ADD_SMURFS
+      , ADD_SMURFS_SUCCESS
+      , ADD_SMURFS_FAILURE      } from './../actions/index';
 
 const initialState = {
   smurfs: [],
@@ -10,9 +16,6 @@ const initialState = {
   deletingSmurf: false,
   error: null,
 }
-
-
-
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -20,3 +23,45 @@ const initialState = {
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export const smurfReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case FETCHING_SMURFS :
+      return {
+        ...state,
+        fetchingSmurfs: true,
+      }
+    case FETCHING_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false,
+      }
+    case FETCHING_SMURFS_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload,
+      }
+
+    case ADD_SMURFS:
+      return {
+        ...state,
+        addingSmurf: true,
+      }
+    case ADD_SMURFS_SUCCESS:
+      return {
+        ...state,
+        addingSmurf: false,
+        smurfs: action.payload,
+      }
+    case ADD_SMURFS_FAILURE:
+      return {
+        ...state,
+        addingSmurf: false,
+        error: action.payload,
+      }
+      default:
+      return state;
+    }}
+
+    export default smurfReducer;
